799ef9d4dc157efd5338a22308da2c771df9cb49
ilyas-11 <ilyassabid11@gmial.com>
2026-07-12 22:07:28 +0100

    scytale_decoder

--- scytale_decoder/src/lib.rs
+++ scytale_decoder/src/lib.rs
@@ -1,24 +1,34 @@
-pub fn scytale_decoder(
-    s: String,
-    letters_per_turn: usize,
-) -> Option<String> {
+//scytale_decoder level:5
+pub fn scytale_decoder(s: String, letters_per_turn: usize) -> Option<String> {
     if s.is_empty() || letters_per_turn == 0 {
         return None;
     }
 
-    let chars: Vec<char> = s.chars().collect();
+    let  chars :Vec<char>=s.chars().collect();
+    let len = chars.len();
 
-    let rows = (chars.len() + letters_per_turn - 1)
-        / letters_per_turn;
+    let mut columns =len / letters_per_turn;
+    if len % letters_per_turn>0{
+        columns+=1;
+    }
+    let mut index =0;
+    let mut grid =vec![vec!['\0';columns];letters_per_turn];
 
+    // ملء المصفوفة عمودًا عمودًا
+    for col in 0..columns{
+        for row in 0..letters_per_turn{
+            if index<len{
+                grid[row][col]=chars[index];
+                index+=1;
+            }
+        }
+    }
+   
     let mut result = String::new();
-
-    for row in 0..rows {
-        for col in 0..letters_per_turn {
-            let index = col * rows + row;
-
-            if index < chars.len() {
-                result.push(chars[index]);
+    for row in 0..letters_per_turn {
+        for col in 0..columns {
+            if grid[row][col] != '\0' {
+                result.push(grid[row][col]);
             }
         }
     }
