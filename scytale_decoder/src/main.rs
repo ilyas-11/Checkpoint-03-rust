dfa280101b204190720380b6bc516b0916738538
ilyas-11 <ilyassabid11@gmial.com>
2026-07-13 04:28:53 +0100

    drop_the_blog

--- scytale_decoder/src/main.rs
+++ scytale_decoder/src/main.rs
@@ -4,7 +4,7 @@ use scytale_decoder::*;
 fn main() {
     println!(
         "\"sec yCtoadle\" size=2 -> {:?}",
-        scytale_decoder("hello worde!".to_string(), 2)
+        scytale_decoder("hello worde".to_string(), 2)
     );
 
     println!(
